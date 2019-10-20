8b5244a5863f36aef992be715ffae9836605f43f
Christopher Teodoro <clt2me@yahoo.com>
2019-10-19 23:35:06 -0500

    adding what I can

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// pushing the random logic as psuedo code got stuck on this one...//
+
+// $( document ).ready(function() {
+//     var yourScore = 0
+//     var randomNum = 0
+//     var wins = 0
+//     var losses = 0 
+//     var crystal1 = 0
+//     var crystal2 = 0
+//     var crystal3 = 0
+//     var crystal4 = 0
+    
+//     var roundStart = function(){
+
+//         crystal1 = Math.floor((Math.random() * 12) + 1);
+//         crystal2 = Math.floor((Math.random() * 12) + 1);
+//         crystal3 = Math.floor((Math.random() * 12) + 1);
+//         crystal4 = Math.floor((Math.random() * 12) + 1);
+
+//         randomNum = Math.floor((Math.random() * 120) + 19);
+//         $("#randomNum").text(randomNum);
+
+//         userScore = 0
+//         $("#yourScore").text(" " + yourScore);
+
+
+
+//         console.log(crystal1);
+//         console.log(crystal2);
+//         console.log(crystal3);
+//         console.log(crystal4);
+//         console.log(randomNum);
+
+//     };
